e51f862df0d68fa67af534b018fca8b38d0b8db6
Igor Gorlov <gorlov35@gmail.com>
2020-03-21 18:16:36 +0300

    add index page

--- markup/assets/js/main.js
+++ markup/assets/js/main.js
@@ -12,7 +12,6 @@ import svg4everybody from 'svg4everybody';
     'background: #2948ff',
     'color: #fff',
     'display: inline-block',
-    'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.2)',
     'box-shadow: 0 -1px 0 rgba(255, 255, 255, 0.2) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset',
     'line-height: 1.56',
     'text-align: left',
@@ -50,29 +49,171 @@ import svg4everybody from 'svg4everybody';
     if( $variationsSlider.length > 0 ) {
 
       $variationsSlider.slick({
-        slidesToShow: 3,
+        // slidesToShow: 3,
         slidesToScroll: 1,
         // initialSlide: 1,
-        arrows: false,
+        arrows: true,
+        dots: false,
         mobileFirst: true,
-        infinite: false,
+        infinite: true,
         adaptiveHeight: true,
-        speed: 100,
+        speed: 300,
+        prevArrow: '.variations__slider-prev',
+        nextArrow: '.variations__slider-next',
+
         responsive: [
           {
             breakpoint: 0,
-            settings: 'unslick'
+            settings: {
+              slidesToShow: 1.2,
+            }
+          },
+          {
+            breakpoint: 375,
+            settings: {
+              slidesToShow: 2,
+            }
           },
           {
             breakpoint: 640,
-            // settings: 'unslick'
+            settings: {
+              slidesToShow: 3,
+            }
           }
         ]
       });
 
+    }
 
+
+
+    var $booksSlider = $('.books__slider');
+
+    if( $booksSlider.length > 0 ) {
+
+      $booksSlider.slick({
+        // slidesToShow: 3,
+        slidesToScroll: 1,
+        // initialSlide: 1,
+        arrows: true,
+        dots: false,
+        mobileFirst: true,
+        infinite: true,
+        adaptiveHeight: true,
+        speed: 300,
+        prevArrow: '.books__slider-prev',
+        nextArrow: '.books__slider-next',
+
+        responsive: [
+          {
+            breakpoint: 0,
+            settings: {
+              slidesToShow: 1.2,
+            }
+          },
+          {
+            breakpoint: 375,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 4,
+            }
+          }
+        ]
+      });
 
     }
+
+
+    var $clientsSlider = $('.clients__slider');
+
+    if( $clientsSlider.length > 0 ) {
+
+      $clientsSlider.slick({
+        // slidesToShow: 3,
+        slidesToScroll: 4,
+        // initialSlide: 1,
+        arrows: false,
+        dots: true,
+        mobileFirst: true,
+        infinite: true,
+        // adaptiveHeight: true,
+        speed: 300,
+
+        responsive: [
+          {
+            breakpoint: 0,
+            settings: {
+              slidesToShow: 1.2,
+            }
+          },
+          {
+            breakpoint: 375,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 6,
+            }
+          }
+        ]
+      });
+
+    }
+
+
+    // Video
+
+    $(document).on('click','.video__button',function(e) {
+      //отменяем стандартное действие button
+      e.preventDefault();
+      var poster = $(this);
+      // ищем родителя ближайшего по классу
+      var wrapper = poster.closest('.video__object');
+      videoPlay(wrapper);
+    });
+
+    //вопроизводим видео, при этом скрывая постер
+    function videoPlay(wrapper) {
+      var iframe = wrapper.find('.video__iframe');
+      // Берем ссылку видео из data
+      var src = iframe.data('src');
+      // скрываем постер
+      wrapper.addClass('video__object_active');
+      // подставляем в src параметр из data
+      iframe.attr('src',src);
+    }
+
+
+
+    // Phone Mask
+
+    // $('.form__field_phone input').mask("+ 7(999)999-99-99", {
+
+    // });
+
+
+    // Books gallery
+
+
+
+    $('.books__item').magnificPopup({
+      type: 'image',
+      gallery:{
+        enabled:false
+      }
+    });
+
+
+
+
 
 
   });
